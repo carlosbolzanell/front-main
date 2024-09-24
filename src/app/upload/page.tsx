b4321e3acc6a7370219b259fe039f12cdf833eeb
JoaoVSantana <joao10victorsantana@gmail.com>
2024-09-24 20:01:47 -0300

    santana

---
+++ src/app/upload/page.tsx
@@ -0,0 +1,13 @@
+
+import InputText from '@/components/input-text/InputText';
+
+export default function Upload() {
+return (
+    <main className="w-full flex flex-col items-center">
+        <section>
+            <h1>Faça upload do seu meme!</h1>
+            <InputText></InputText>
+        </section>
+    </main>
+);
+}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,10 +5,10 @@ import { LuPizza } from "react-icons/lu";
 
 export default function Header() {
   return (
-    <header className="bg-primaria h-16 flex items-center">
+    <header className="bg-azul h-16 flex items-center">
       <div className="flex flex-row justify-between w-[80%] mx-auto items-center">
         <div className="font-kalam text-white flex flex-col items-center">
-          <h1>Hungria</h1>
+          <h1>Lecajovida</h1>
           <h1 className="-mt-1">Pizzas</h1>
         </div>
         <nav className="flex gap-2">
